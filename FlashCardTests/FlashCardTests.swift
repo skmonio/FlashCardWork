8f963310415fff35b01603941d051191289a1394
skmonio <stephenjohncook@outlook.com>
2025-06-04 12:23:51 +0200

    Initial Commit

---
+++ FlashCardTests/FlashCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardTests.swift
+//  FlashCardTests
+//
+//  Created by Stephen Cook on 04/06/2025.
+//
+
+import Testing
+@testable import FlashCard
+
+struct FlashCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
